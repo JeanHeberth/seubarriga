28ec9125150ad6efe5e09f994032c045ba2f5f57
jeanheberth <jeanheberth19@gmail.com>
2025-06-06 15:29:04 -0300

    Foi realizado refatoração nos testes de login, criado métodos para melhorar o teste e a validação de acesso.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,24 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/***
+ * @param {string} email
+ * @param {string} senha
+ * Comando personalizado para realizar o login
+ */
+Cypress.Commands.add('realizarLogin', (email, senha) => {
+    cy.visit('/login');
+    cy.get('#email').type(email);
+    cy.get('#senha').type(senha);
+    cy.get('.btn').click();
+});
+
+/**
+ *
+ * @param {string} mensagemEsperada
+ * @param {string} tipo
+ * Comando personalizado para validar a mensagem
+ */
+Cypress.Commands.add('validarAlerta', (mensagemEsperada, tipo = 'success') => {
+    const classe =  `.alert-${tipo}`
+    cy.get(classe)
+        .should('be.visible')
+        .and('contain', mensagemEsperada);
+});
