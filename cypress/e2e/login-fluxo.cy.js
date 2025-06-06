06c71ddaa7c2205ac0abd7c3915bd33bd9e51962
jeanheberth <jeanheberth19@gmail.com>
2025-06-06 19:04:03 -0300

    realizado melhoria do fluxo de login

--- cypress/e2e/login-fluxo.cy.js
+++ cypress/e2e/login-fluxo.cy.js
@@ -1,28 +1,20 @@
-import { faker } from '@faker-js/faker';
+import {gerarUsuarioFake} from "../support/fakeUser";
 
 describe('Fluxo completo de Login e cadastro de usuário', () => {
-
     it('Deve validar o fluxo completo: cadastro, erro de login e login com sucesso', () => {
 
-        const name = faker.person.fullName();
-        const email = faker.internet.email();
-        const password = faker.internet.password();
+        const usuario = gerarUsuarioFake();
 
         // Login com usuário não cadastrado
-        cy.realizarLogin(email, password);
+        cy.realizarLogin(usuario.email, usuario.senha);
         cy.validarAlerta('Problemas com o login do usuário', 'danger');
 
         // Cadastro de usuário
-        cy.visit('/login');
-        cy.contains('Novo usuário?').click();
-        cy.get('#nome').type(name);
-        cy.get('#email').type(email);
-        cy.get('#senha').type(password);
-        cy.get('.btn').click();
+        cy.cadastrarUsuario(usuario);
         cy.validarAlerta('Usuário inserido com sucesso', 'success');
 
         // Login com sucesso
-        cy.realizarLogin(email, password);
+        cy.realizarLogin(usuario.email, usuario.senha);
         cy.validarAlerta('Bem vindo');
 
     });
